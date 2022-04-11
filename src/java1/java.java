b75e31ad5ecfb6c811884a46d940148156d27bbb
Triple Baconator <triple@baconator.on.ca>
2022-04-11 16:11:41 -0700

    commit

--- src/java1/java.java
+++ src/java1/java.java
@@ -5,7 +5,9 @@ public class java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("welcome");
-	System.out.print("code");
+	System.out.println("code");
+	System.out.println("hi");
+	
 	}
 	
 
